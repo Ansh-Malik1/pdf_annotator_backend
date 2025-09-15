0c8cb15e5b171d46455b62fc91d7307b83f12908
Ansh Malik <anshmalik718@gmail.com>
2025-09-15 20:21:56 +0530

    added authorization routes and JWT authentication. Basic testing also carried out using postman.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./config/database.js";
-
+import authRoutes from "./routes/authRoutes.js";
 dotenv.config();
 connectDB();
 
 const app = express();
-
-
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Backend is working ");
 });
+app.use("/api/auth", authRoutes);
+
 
 export default app;
